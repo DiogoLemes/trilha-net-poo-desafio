81a97f32488d615173fe73b031b4f191329e8729
DiogoLemes <diogo.s.lemes@unesp.br>
2025-07-31 14:08:08 -0300

    Desafio Concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("----------------------");
+
+Smartphone nokia1 = new Nokia("11912345678", "Nokia3310", "123456789012345", 64);
+nokia1.Ligar();
+nokia1.ReceberLigacao();
+nokia1.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("----------------------");
+
+Smartphone iphone1 = new Iphone("11912345678", "Iphone 15", "123456789012345", 64);
+iphone1.Ligar();
+iphone1.ReceberLigacao();
+iphone1.InstalarAplicativo("Google Chrome");
+
+Console.WriteLine("----------------------");
